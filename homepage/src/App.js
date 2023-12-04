258e729e8cee10654e3e0a2963ee7a78ebd0d93c
vireshsoedhwa <vireshsoedhwa@gmail.com>
2023-12-04 02:31:14 -0800

    fix docker compose file

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -27,19 +27,42 @@ function App() {
           </svg>
           <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
             <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Viresh Soedhwa
+              test 1
               {/* <br /> */}
             </h2>
             <p className="mt-6 text-lg leading-8 text-gray-300">
-              This is a test site #1
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
-              {/* <a
+              <a
                 href="#"
                 className="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
-                Get started
-              </a> */}
+                a
+              </a>
+              <a
+                href="#"
+                className="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                b
+              </a>
+              <a
+                href="#"
+                className="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                c
+              </a>
+            </div>
+            <br />
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              test 2
+            </h2>
+            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+              <a
+                href="#"
+                className="rounded-md bg-white px-3.5 py-1.5 text-base font-semibold leading-7 text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+              >
+                a
+              </a>
             </div>
           </div>
           {/* <div className="relative mt-16 h-80 lg:mt-8">
